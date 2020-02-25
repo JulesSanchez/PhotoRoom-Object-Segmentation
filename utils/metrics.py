3ab0cd88d7795cb6342eb0791354e862732074fc
ManifoldFR <manifoldfr@outlook.com>
2020-02-25 10:44:13 +0100

    add soft dice loss

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 # Source: https://gist.github.com/brunodoamaral/e130b4e97aa4ebc468225b7ce39b3137
@@ -40,7 +41,7 @@ def dice_score(im1, im2, empty_score=1.0):
     return 2. * intersection.sum() / im_sum
 
 def score_sample(masks_proposed, mask_truth):
-    return np.mean([dice_score(masks_proposed[i],mask_truth[i]) for i in range(len(mask_proposed))])
+    return np.mean([dice_score(masks_proposed[i],mask_truth[i]) for i in range(len(masks_proposed))])
 
 
 class CrossEntropyLoss2d(nn.Module):
@@ -50,3 +51,40 @@ class CrossEntropyLoss2d(nn.Module):
 
     def forward(self, inputs, targets):
         return self.nll_loss(F.log_softmax(inputs), targets)
+
+EPSILON = 1e-8
+
+
+def soft_dice_loss(input: torch.Tensor, labels: torch.Tensor, softmax=True) -> torch.Tensor:
+    """Mean soft dice loss over the batch. From Milletari et al. (2016) https://arxiv.org/pdf/1606.04797.pdf 
+    
+    Parameters
+    ----------
+    input : Tensor
+        (N,C,H,W) Predicted classes for each pixel.
+    labels : LongTensor
+        (N,C,H,W) Tensor of pixel labels.
+    softmax : bool
+        Whether to apply `F.softmax` to input to get class probabilities.
+    """
+    labels = F.one_hot(labels).permute(0, 3, 1, 2)
+    dims = (1, 2, 3)  # sum over C, H, W
+    if softmax:
+        input = F.softmax(input, dim=1)
+    intersect = torch.sum(input * labels, dim=dims)
+    denominator = torch.sum(input**2 + labels**2, dim=dims)
+    ratio = intersect / (denominator + EPSILON)
+    return torch.mean(1 - 2. * ratio)
+
+
+class CombinedLoss(nn.Module):
+    """Combined cross-entropy + soft-dice loss."""
+
+    def __init__(self, weight: torch.Tensor = None):
+        super().__init__()
+        self.weight = weight
+
+    def __call__(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        """CE + Dice"""
+        return F.cross_entropy(input, target, reduction="mean", weight=self.weight) \
+            + soft_dice_loss(input, target)
